9e9e8b08c4f6451fe0370ad97f515baf80cbc639
Markus Fink <markus.fink@massiveart.com>
2018-05-05 00:56:59 +0200

    add player

--- assets/js/containers/NewGameForm/NewGameForm.js
+++ assets/js/containers/NewGameForm/NewGameForm.js
@@ -9,6 +9,11 @@ import GameStore from '../../stores/GameStore';
 @observer
 export default class NewGameForm extends React.Component {
     @observable playerName;
+    @observable players = [
+        {
+            'name': '',
+        },
+    ];
 
     handleSubmit = (event) => {
         event.preventDefault();
@@ -18,23 +23,39 @@ export default class NewGameForm extends React.Component {
     handleChangeGame = (event) => {
     };
 
-    handlePlayerNameChange = (playerName) => {
-        this.playerName = playerName;
+    handlePlayerNameChange = (playerName, index) => {
+        this.players[index].name = playerName;
     };
 
     handleAddPlayer = (event) => {
+        this.players.push({'name': ''});
     };
+
+    renderPlayerInputs = () => {
+        let inputs = [];
+        for (let i = 0; i < this.players.length; i++) {
+            inputs.push(
+                <Input
+                    onChange={(playerName) => { this.handlePlayerNameChange(playerName, i); }}
+                    key={'playerName_' + i}
+                    name={'playerName_' + i}
+                    value={this.players[i].name}
+                />);
+        }
+
+        return inputs;
+    }
 
     render() {
         return (
             <div>
                 {
                     <form onSubmit={this.handleSubmit}>
-                        <Select onChange={this.handleChangeGame} name="game" />
+                        <Select onChange={this.handleChangeGame} name='game' />
 
-                        <Input onChange={this.handlePlayerNameChange} name="playerName" value={this.playerName} />
+                        {this.renderPlayerInputs()}
                         <Button type="button" onClick={this.handleAddPlayer}>
-                            +
+                            Add Player
                         </Button>
 
                         <Button type="submit">
